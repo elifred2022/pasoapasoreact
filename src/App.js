68c86ea95a9dece5c7a5d774dd44df152f6dd633
elifred2022 <elifed21@hotmail.com>
2023-09-05 19:28:39 -0300

    empezando paso a paso

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Congratulations from "./Desafios";
+import Gallery from "./Importexport";
+
+export default function App() {
+  return (
+    <div>
+      <Congratulations />
+      <Gallery />
+    </div>
+  );
+}
+
+// https://es.react.dev/learn/importing-and-exporting-components
---
+++ src/Desafios.js
@@ -0,0 +1,62 @@
+/*
+function Profile() {
+  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
+}
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Cientificos increibles</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
+*/
+
+// desafio 1
+/*
+//export default// function Profile() {
+  return <img src="https://i.imgur.com/lICfvbD.jpg" alt="Aklilu Lemma" />;
+}
+ */
+
+// desafio 2
+/*
+export default function Profile() {
+  return (
+    <div>
+      <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />;
+    </div>
+  );
+} */
+
+// desafio 3
+
+/*
+
+function Profile() {
+  return <img src="https://i.imgur.com/QIrZWGIs.jpg" alt="Alan L. Hart" />;
+}
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Científicos increíbles</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+} */
+
+// desafio 4 crear mi propio componente
+
+export default function Congratulations() {
+  return (
+    <section>
+      <h1>¡Buen trabajo!</h1>
+    </section>
+  );
+}
